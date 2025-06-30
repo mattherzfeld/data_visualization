9ef16afe5b151e2680db85e0d0f22e16b6abbc4f
mattherzfeld <matthew.herzfeld@gmail.com>
2025-06-29 21:03:48 -0500

    Examples with matplotlib
    Examples with plotting a line  versus scatter plot

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
 
-plt.style.use('Solarize_Light2') # Use 'plt.style.available' to see other available plotting styles
+plt.style.use('_classic_test_patch')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+ax.scatter(x_values, y_values, s=100)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
