26017e4d03f3c1645e17e03b7f31733512b880c3
mattherzfeld <matthew.herzfeld@gmail.com>
2025-06-30 23:59:17 -0500

    More plotting
    More plotting techniques (basic) - saves to file instead of plotting in the viewer.

--- scatter_squares.py
+++ scatter_squares.py
@@ -5,7 +5,10 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('_classic_test_patch')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, color=(0, 0.8, 0), s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
+#Color options
+# color='red'
+# color=(0, 0.8, 0)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -13,10 +16,14 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set the range for each axis.
-ax.axis([0, 1100, 0, 1_100_000])
+ax.axis([0, 1100, 0, 1_100_000])    
 ax.ticklabel_format(style='plain')
 
 # Set size of tick labels.
 ax.tick_params(labelsize=14)
 
-plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight') #Saves plot to a file
+
+#plt.show()
+#The above shows in the Matplotlib viewer
+
