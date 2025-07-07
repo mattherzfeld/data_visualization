b60d2c1ea9f3bcd9eacacdfd6c98951dd85b5d0e
mattherzfeld <matthew.herzfeld@gmail.com>
2025-07-06 23:39:38 -0500

    Beginning/Ending Points
    Emphasize the beginning point (green) and ending point (red).

--- rw_visual.py
+++ rw_visual.py
@@ -16,6 +16,11 @@ while True:
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
     ax.set_aspect('equal')
+
+    # Emphasize the first and last points.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
     plt.show()
 
     # Otherwise this would keep running the above code indefinitely...
