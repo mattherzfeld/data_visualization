206654987fb9c308a92e6f73dba9e67433c2b029
mattherzfeld <matthew.herzfeld@gmail.com>
2025-07-06 23:24:27 -0500

    Multiple random walks update
    Generate multiple random walks... then give the user the option to quit.

--- rw_visual.py
+++ rw_visual.py
@@ -2,14 +2,22 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Make a random walk.
+# Keep making new walks, as long as the program is active.
 
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    # Make a random walk.
 
-# Plot the points in the walk.
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal')
-plt.show()
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    # Plot the points in the walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    ax.set_aspect('equal')
+    plt.show()
+
+    # Otherwise this would keep running the above code indefinitely...
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
