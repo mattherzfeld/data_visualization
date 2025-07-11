739873b37985a37d9f23fcde27bf0a89eb3a41d8
mattherzfeld <matthew.herzfeld@gmail.com>
2025-07-11 00:09:13 -0500

    Die Rolling VIsualization
    Creating  a visualization of rolling a die.

---
+++ die_visual.py
@@ -0,0 +1,12 @@
+from die import Die
+
+# Create a die.
+die = Die()
+
+# Make some rolls, and store results in a list.
+results = []
+for roll_num in range(100):
+    result = die.roll()
+    results.append(results)
+
+print(results)
